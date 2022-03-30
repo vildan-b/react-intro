5886fef788097171f6b04f5c152936fd22df3c32
vildan-b <vildanb@gmx.de>
2022-03-30 15:19:31 +0200

    State

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,24 +3,28 @@ import { ListGroup,ListGroupItem } from "reactstrap";
 
 
 export default class CategoryList extends Component {
+constructor(props){
+  super(props);
+  this.state= {categories:[{categoryId:1,categoryName:"Computer"},
+                           {categoryId:2,categoryName:"Elektronik"}
+]};
+
+}
 
   render() {
     return (
       <div>
         <h5>{this.props.info.title}</h5>
+        <h5>{this.state.counter}</h5>
         <ListGroup>
-  <ListGroupItem color="success">
-    Cras justo odio
-  </ListGroupItem>
-  <ListGroupItem color="info">
-    Dapibus ac facilisis in
-  </ListGroupItem>
-  <ListGroupItem color="warning">
-    Morbi leo risus
-  </ListGroupItem>
-  <ListGroupItem color="danger">
-    Porta ac consectetur ac
-  </ListGroupItem>
+          {
+            this.state.categories.map(category =>(
+              <ListGroupItem color="danger" key={category.categoryId}>
+                {category.categoryName}</ListGroupItem>
+
+            ))
+          }
+  
 </ListGroup>
       </div>
     )
